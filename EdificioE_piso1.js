751478bb63e0b8b5951dc52cef12833ed1675514
david <tdavidvergara2004@gmail.com>
2023-11-21 12:46:59 -0500

    se integró todo definitivamente, para proceder a reservas salas

--- EdificioE_piso1.js
+++ EdificioE_piso1.js
@@ -1,55 +1,44 @@
-    var salaCardContainer = document.getElementById('salaCardContainer');
-    var salones = document.getElementById('salones') ;
+var salaCardContainer = document.getElementById('salaCardContainer');
 
-
-    /*if (window.localStorage.getItem('usuario') === null) {
-        window.location.href = "/Registro.html";
-    } else {
-        var user = JSON.parse(window.localStorage.getItem('usuario'));
-    }*/
-    async function getSalas(){
-
-        let edificio = window.localStorage.getItem('Edificio');
-        if(edificio === "Ver Salones Disponibles del E"){
-            var edificioId = 'E' ;
-        }else if(edificio === "Ver Salones Disponibles del B"){
-
-            var edificioId = 'B' ;
-
-        }
-        let response = await fetch('http://127.0.0.1:8080/Salasicesi/salones/'+ edificioId,{
-            method: 'GET',
-            headers: {
-                'Authorization': '123'
+        async function getSalas() {
+            let edificio = window.localStorage.getItem('Edificio');
+            if (edificio === "Ver Salones Disponibles del E") {
+                var edificioId = 'E';
+            } else if (edificio === "Ver Salones Disponibles del B") {
+                var edificioId = 'B';
             }
-        });
-            if(response.status ===200){
-
-            let json = await response.json();    
-            json.forEach(salon => {
-                var card = new SalaCard( salon );
-                salaCardContainer.appendChild(card.render());    
+            let response = await fetch('http://127.0.0.1:8080/Salasicesi/salones/' + edificioId, {
+                method: 'GET',
+                headers: {
+                    'Authorization': '123'
+                }
             });
+            if (response.status === 200) {
+                let json = await response.json();
+                json.forEach(salon => {
+                    var card = new SalaCard(salon);
+                    salaCardContainer.appendChild(card.render());
+                });
 
-
-        } else{
-            alert(await response.text())
+                // Después de agregar las salas al contenedor, configura los event listeners
+                setupClickHandlers();
+            } else {
+                alert(await response.text());
+            }
         }
-    
-    }
 
-    getSalas();
+        getSalas();
 
+        function setupClickHandlers() {
+            var salones = document.querySelectorAll('#salones');
+            salones.forEach(function (salonElemento) {
+                salonElemento.addEventListener('click', manejarClicSalon);
+            });
+        }
 
-    function manejarClicSalon(event) {
-        event.preventDefault();
-        var salonSeleccionado = event.target.innerHTML;
-        window.localStorage.setItem("sala", salonSeleccionado);
-        window.location.href = "/HorarioDisponible.html";
-    }
-
-    salones.forEach(function (salonId) {
-        var salonElemento = document.getElementById(salonId);
-        salonElemento.addEventListener('click', manejarClicSalon);
-    });
-
+        function manejarClicSalon(event) {
+            event.preventDefault();
+            var salonSeleccionado = event.target.innerHTML;
+            window.localStorage.setItem("sala", salonSeleccionado);
+            window.location.href = "/HorarioDisponible.html";
+        }
---
+++ MenuPrincipal.js
@@ -0,0 +1,7 @@
+var user = localStorage.getItem('user');
+    if (user === null) {
+        window.location.href = "/Registro.html";
+        console.log(user) ;
+    } else {
+        var user = JSON.parse(window.localStorage.getItem(user));
+    }
