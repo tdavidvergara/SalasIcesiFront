ad80215c89fde4c6a1e90c2945c408a033791d02
david <tdavidvergara2004@gmail.com>
2023-11-22 09:59:26 -0500

    Asignar Sala HTML

--- HorarioDisponible.js
+++ HorarioDisponible.js
@@ -1,45 +1,85 @@
-const item1 = document.getElementById('item1');
-const item2 = document.getElementById('item2');
-const item3 = document.getElementById('item3');
-const imagenSalon = document.getElementById('imagenSalon') ;
+const imagenSalon = document.getElementById('imagenSalon');
 const roomDetail = document.getElementById('roomDetail');
+const guardarButton = document.getElementById('guardar');
+const sala = window.localStorage.getItem('sala');
+const fechaInput = document.getElementById('fecha');
+let horaSeleccionada ;
 
 
+var user = localStorage.getItem('user');
 
-if(window.localStorage.getItem('user') === null){
-
-    window.location.href = "/Registro.html" ;
-
-}else{
-
-    var user = JSON.parse(window.localStorage.getItem('user')) ;
-
+if (user === null) {
+    window.location.href = "/Registro.html";
+} else {
+    var user = JSON.parse(window.localStorage.getItem(user));
 }
 
-console.log(user); 
+console.log(user);
 
-async function getInformation(){
-    let sala = window.localStorage.getItem('sala');
-    
-    let response = await fetch('http://localhost:8080/salasIcesi/informacion/'+sala,{
-        method: 'GET',headers: {
+async function getInformation() {
+    let response = await fetch('http://localhost:8080/salasIcesi/informacion/' + sala, {
+        method: 'GET',
+        headers: {
             'Authorization': '123'
         }
     });
-    if(response.status ===200){
-
+    if (response.status === 200) {
         let json = await response.json();
         console.log(json);
         var card = new RoomCard(json);
         console.log(card.render());
-        roomDetail.appendChild(card.render());    
-
-
-    } else{
-        alert(await response.text())
+        roomDetail.appendChild(card.render());
+        setupHoraButtons() ; 
+        alert(await response.text());
     }
-   
 }
 
-getInformation();
 
+function setupHoraButtons() {
+    const horaButtons = document.querySelectorAll('cambiarColor');
+
+    horaButtons.forEach(function (boton) {
+        boton.addEventListener('click', function (event) {
+            horaSeleccionada = event.target.innerHTML;
+            cambiarColor(boton);
+            console.log('Hora seleccionada:', horaSeleccionada);
+        });
+    });
+}
+
+guardarButton.addEventListener('click', async function () {
+    // Obtenemos la hora seleccionada
+         // Creamos un objeto con la información de la sala
+    var gestionSalaDTO = {
+        idSala: sala,
+        idUsuario: 1    , // Utilizamos el ID del usuario obtenido del local storage
+        dia: fechaInput,
+        hora: horaSeleccionada,
+    };
+
+    // Convertimos el objeto en un string JSON
+    var json = JSON.stringify(gestionSalaDTO);
+
+    // Enviamos la solicitud POST al servidor con los datos del objeto
+    let response = await fetch('http://localhost:8080/salasIcesi/reservarSala', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': '123'
+        },
+        body: json
+    });
+
+    // Procesamos la respuesta del servidor
+    if (response.status === 200) {
+        let respuesta = await response.text();
+        alert(respuesta); // Mostramos el mensaje de éxito
+    } else {
+        alert(await response.text()); // Mostramos el mensaje de error
+    }
+    
+
+   
+});
+
+getInformation();
