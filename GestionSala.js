b131637ec4a9497eeee4bdd59f233094e4a8995a
david <tdavidvergara2004@gmail.com>
2023-11-30 17:36:45 -0500

    como admin puedo habilitar de estudio

--- GestionSala.js
+++ GestionSala.js
@@ -4,6 +4,9 @@ var salaId = localStorage.getItem('salaId') ;
 var InhabilitarSalaButton = document.getElementById('InhabilitarSala');
 var modalConfirmacion = new bootstrap.Modal(document.getElementById('modalConfirmacion'));
 var cerrar = document.getElementById("cerrar") ;
+var cerrarModalConfirmacion = document.getElementById("cerrarModalConfirmacion") ;
+var habilitarButton = document.getElementById('habilitarButton')  ;
+var modalConfirmacionHabilitar = new bootstrap.Modal(document.getElementById('modalConfirmacionHabilitar'))  ;
 
 console.log (salaId) ;
 
@@ -44,3 +47,33 @@ inhabilitarSala.addEventListener('click', async function(event){
       
 
 }) ;
+
+habilitarButton.addEventListener('click', async function(event){
+    event.preventDefault();
+    try{
+
+        let response = await fetch('http://localhost:8080/salasIcesi/administrador/habilitar/'+salaId, {
+            method: 'PUT',
+            headers: {
+                'Authorization': '1'
+            }
+        });
+
+        if (response.status === 200) {
+            modalConfirmacionHabilitar.show();
+            cerrarModalConfirmacion.addEventListener('click' , function(){
+            window.location.href  = "/SolicitudCompletada.html"  ;
+            }) ;
+             }else{
+            alert('No se pudo realizar la solicitud') ;
+            window.location.reload() ;
+        }
+    
+    }catch{
+        alert('No se pudo realizar la solicitud') ;
+        window.location.reload() ;
+    }
+      
+
+}) ;
+
