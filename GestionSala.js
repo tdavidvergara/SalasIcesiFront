1948a843a0f055c95d50a60fc74b0d1cef5ddce4
david <tdavidvergara2004@gmail.com>
2023-11-30 09:13:30 -0500

    se arreglaron unos errores

---
+++ GestionSala.js
@@ -0,0 +1,11 @@
+var InhabilitarSala = document.getElementById('InhabilitarSala')  ;
+var user = localStorage.getItem('user');
+
+
+if (user === null) {
+    window.location.href = "/Registro.html";
+
+} else {
+    user = JSON.parse(user);
+    console.log(user) ;
+}
